55b5d166a77b6b5a9ab7ee6aa192c1a8672584ea
Carron White <carronwhite@gmail.com>
2019-12-23 16:11:35 -0500

    Apparently babel config was not complete

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]]
+  presets: [["@babel/preset-env", { targets: { node: "current" } }],
+  "next/babel"],
+  plugins: ["@babel/plugin-syntax-dynamic-import"]
 }
