6365ecc22f9cd4ba70d026c6b353423bc9149001
Carron <carronwhite@gmail.com>
2020-05-07 01:17:39 -0500

    CSS/Sass now built in, wow

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
-module.exports = withSass(withCSS(
-  {
-    webpack(config) {
-       config.node = { fs: 'empty' }
-       return config
-    },
-    onDemandEntries: {
-      // Make sure entries are not getting disposed.
-      maxInactiveAge: 1000 * 60 * 60
-    }
+module.exports = {
+  webpack(config) {
+     config.node = { fs: 'empty' }
+     return config
+  },
+  onDemandEntries: {
+    // Make sure entries are not getting disposed.
+    maxInactiveAge: 1000 * 60 * 60
   }
-))
+}
