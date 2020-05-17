2fc14ab5e39527986e163f9c80852eda19b8d54b
Carron <carronwhite@gmail.com>
2020-05-17 09:52:49 -0500

    Test api in staging

--- routes/profile.js
+++ routes/profile.js
@@ -48,11 +48,30 @@ router.post('/username', function(req, res) {
   })
 })
 
+// router.get('/artists', function(req, res) {
+//   const { offset, limit } = req.query
+//   ArtistProfile.paginate({}, { offset: parseInt(offset), limit: parseInt(limit) }).then(function(result) {
+//     res.json(result)
+//   })
+// })
+
+// Test
 router.get('/artists', function(req, res) {
-  const { offset, limit } = req.query
-  ArtistProfile.paginate({}, { offset: parseInt(offset), limit: parseInt(limit) }).then(function(result) {
-    res.json(result)
-  })
+  const result = { expertise: { hair: [], makeup: []},
+sessions: [],
+isArtist: true,
+_id: "5e95268842c452e8400b5e47",
+username: "ruth",
+displayName: "Ruth Shaw Beauty",
+profileImageUrl: "http://res.cloudinary.com/daqyhasiw/image/upload/v1586834497/cojlexxsooavxueny1lf.png",
+youtube: "rshawbeauty",
+instagram: "",
+twitter: "",
+facebook: "",
+hourlyRate: 25
+}
+
+    res.json({docs: [result]})
 })
 
 router.post('/artist', function(req, res) {
