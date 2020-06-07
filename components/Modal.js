71bd6bec6af2a68332cb5d3f8bc584052fc00ebc
Carron <carronwhite@gmail.com>
2020-06-07 13:15:14 -0500

    Modal changes

--- components/Modal.js
+++ components/Modal.js
@@ -1,16 +1,22 @@
-import ReactDOM from 'react-dom'
+import { createPortal } from 'react-dom'
+import { useRef, useEffect, useState } from 'react'
 const cn = require('classnames')
 
-const Modal = (props) => (
-  ReactDOM.createPortal(
+const Modal = (props) => {
+  const [ mounted, setMounted ] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return mounted ? createPortal(
     <div className='modal'>
-    <div onClick={ props.closeModal } className="close">✕</div>
       <div className={cn('modal-content', props.layout)}>
+        <div onClick={ props.closeModal } className="close">✕</div>
         {props.children}
       </div>
-    </div>,
-    document.body
-  )
-)
+    </div>, document.body
+  ) : null
+}
 
 export default Modal
